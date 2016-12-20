205329b6057740d90911ae94d73fa851133c4c67
georgedr1 <georgedr@rose-hulman.edu>
2016-12-19 20:33:24 -0500

    update

--- IdenticalWebcamTest.py
+++ IdenticalWebcamTest.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec 12 19:15:29 2016
-
 This script will be used for our "Identical Webcam" test.
-
 The purpose of this will be to determine if our webcams are acceptably identical or not.
 If they are different, we will need to know the differences that make them distinct.
-
 @author: mcdanitd
 """
 
@@ -15,7 +12,7 @@ import numpy as np
 #import antigravity
 
 vc1 = cv2.VideoCapture(1)
-vc2 = cv2.VideoCapture(2)
+vc2 = cv2.VideoCapture(1)
 
 if vc1.isOpened(): # try to get the first frame
     rval1, frame1 = vc1.read()
@@ -28,13 +25,14 @@ else:
     rval2 = False
 
 if rval1 == False:
-    sys.exit("Camera 1 did not open correctly.")
+    exit("Camera 1 did not open correctly.")
 if rval2 == False:
-    sys.exit("Camera 2 did not open correctly.")
+    exit("Camera 2 did not open correctly.")
 
 cam1 = 0    # 
 for i in range(0,5):    #collect data from first camera
     rval1, frame1 = vc1.read()
+    frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
     cam1 = cam1 + frame1
 
 print("Program paused. Move second camera into place.")
@@ -43,11 +41,24 @@ raw_input()
 cam2 = 0
 for j in range(0,5):    #collect data from second camera
     rval2, frame2 = vc2.read()
+    frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
     cam2 = cam2 + frame2
+
 
 avgCam1 = cam1/i
 avgCam2 = cam2/j
 
-difference = avgCam1 - avgCam2    #difference between the two cameras
-percentDiff = diff/frame1
-  
+
+count = 0
+h, w = cam1.shape[:2]
+for x in range(0,w):
+    for y in range(0,h):
+        if abs(cam1[y,x,1]-cam2[y,x,1])<=100:
+            count = count + 1
+
+print h*w
+print count
+
+
+#diff = avgCam1 - avgCam2    #difference between the two cameras
+#percentDiff = diff/frame1
--- main.py
+++ main.py
@@ -23,102 +23,153 @@ depthMaxVertAngle = 66
 separation = 0.04269      #distance between cameras
 print "Starting feed display"
 depthArray = depth.getImage()
-depthArray1 = numpy.zeros(depthArray.shape[:2])
-depthArray2 = numpy.zeros(depthArray.shape[:2])
-depthArray3 = numpy.zeros(depthArray.shape[:2])
+depthArray1 = depth.getImage()
+depthArray2 = depth.getImage()
+depthArray3 = depth.getImage()
 A = numpy.zeros(depthArray.shape[:2])
-#while(1):
-for jrog in range(0,6):
-   
+loops = 0
+successHistory = 0
+mid50SuccessHistory = 0
+mid25SuccessHistory = 0
+
+#depthimagefile = 0;
+
+while(1):
+#for jrog in range(0,6):
+    
+    loops+=1
         
     depthArray = depth.getImage()
     left = feed.getImage(1)
     right = feed.getImage(2)
     
-    left = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)
-    right = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('DepthImage', depthArray)
-    cv2.imshow('Left', left)
-    cv2.imshow('Right', right)
+    left = cv2.cvtColor(left, cv2.COLOR_RGB2GRAY)
+    right = cv2.cvtColor(right, cv2.COLOR_RGB2GRAY)
     
     depthArray4 = depthArray3
     depthArray3 = depthArray2
     depthArray2 = depthArray1
     depthArray1 = depthArray
     
-    depthArray = (depthArray1+depthArray2+depthArray3+depthArray4)/4
+    depthArray = (depthArray1*4+depthArray2*3+depthArray3*2+depthArray4)/10
+    
+#    depthArray = cv2.fromarray(depthArray)
+#    depthArray = cv2.medianBlur(depthArray,3)
+    
+    cv2.imshow('DepthImage', depthArray)
+    cv2.imshow('Left', left)
+    cv2.imshow('Right', right)
+    
     
     webcamRows, webcamColumns = left.shape[:2]    
 #    leftCenterline = left[math.floor(h/2)]
     
-    h, w = right.shape[:2]    
+    h, w = right.shape[:2]
 #    rightCenterline = right[math.floor(h/2)]
     
     hArray, wArray = depthArray.shape[:2]    
 #    dAcenterline = depthArray[math.floor(h1/2)]
     
-    
-    print "sleeping"
-    print jrog
-    time.sleep(1)
+#    
+#    print "sleeping"
+#    print jrog
+#    time.sleep(1)
     success = 0
-    if jrog == 5:
-        leftMap = numpy.zeros((hArray,wArray,2))
-        rightMap = numpy.zeros((hArray,wArray,2))
-    
-        compArray = numpy.zeros((2,hArray,wArray))
-        col=-1
-        for r in range(0,hArray):         
-            VertAngle = (r/hArray)*depthMaxVertAngle + (cameraMaxVertAngle-depthMaxVertAngle)/2
-            lWebcamYValue = webcamRows*VertAngle/cameraMaxVertAngle
-            rWebcamYValue = webcamRows*VertAngle/cameraMaxVertAngle
-                    
-            for c in range(0,wArray): 
-                y = depthArray[r,c]   
-                
-                A = ((float(c)/wArray)*depthMaxHorzAngle)-(depthMaxHorzAngle/2)
-               
-#                x=y*math.cos(math.radians(A))
-                x = y
-                C = A + 90      
-                wL = math.sqrt(math.pow(x,2)+math.pow(separation,2) - 2*x*separation*math.cos(math.radians(C)))
-                FL = math.degrees(math.asin(separation*math.sin(math.radians(C))/wL))
-                DL = 180 - FL -C
-                BL = 90 - DL
-                lWebcamXValue = (BL + 45)*webcamColumns/cameraMaxHorzAngle
+    middle50Success = 0
+    middle25Success = 0
+#    if jrog == 5:
+    leftMap = numpy.zeros((hArray,wArray,2))
+    rightMap = numpy.zeros((hArray,wArray,2))
+    leftInverseMap = numpy.zeros((webcamRows,webcamColumns,1))
+    rightInverseMap = numpy.zeros((webcamRows,webcamColumns,1))
 
-                if (lWebcamXValue <= 640) and (lWebcamXValue>=0):
-                    leftMap[r,c,0] = lWebcamYValue
-                    leftMap[r,c,1] = lWebcamXValue
-                    compArray[0,r,c] = left[lWebcamYValue,lWebcamXValue]
-                E = 180 - C
-                wR = math.sqrt(math.pow(x,2)+math.pow(separation,2) - 2*x*separation*math.cos(math.radians(E)))
-                FR = math.degrees(math.asin(separation*math.sin(math.radians(E))/wR))
-                DR = 180 - FL - E
-                BR = DR - 90
-                rWebcamXValue = (BR + 45)*webcamColumns/cameraMaxHorzAngle
-                if rWebcamXValue <= 640 and rWebcamXValue>=0:
-                    rightMap[r,c,0] = rWebcamYValue
-                    rightMap[r,c,1] = rWebcamXValue
-                    compArray[1,r,c] = right[rWebcamYValue,rWebcamXValue]
-                if abs(compArray[0,r,c]-compArray[1,r,c]<50):
-                    success+=1;
+    errors = numpy.zeros((hArray,wArray,1))
+    
+    compArray = numpy.zeros((2,hArray,wArray), dtype=numpy.uint8)
+    midCompArray = numpy.zeros((2,hArray/2,wArray/2))
+    col=-1
+    for r in range(0,hArray):         
+        VertAngle = (r/hArray)*depthMaxVertAngle + (cameraMaxVertAngle-depthMaxVertAngle)/2
+        lWebcamYValue = webcamRows*VertAngle/cameraMaxVertAngle
+        rWebcamYValue = webcamRows*VertAngle/cameraMaxVertAngle
+                
+        for c in range(0,wArray): 
+            y = depthArray[r,c]   
+            
+            A = ((float(c)/wArray)*depthMaxHorzAngle)-(depthMaxHorzAngle/2)
+           
+#                x=y*math.cos(math.radians(A))
+            x = y
+            C = A + 90      
+            wL = math.sqrt(math.pow(x,2)+math.pow(separation,2) - 2*x*separation*math.cos(math.radians(C)))
+            FL = math.degrees(math.asin(separation*math.sin(math.radians(C))/wL))
+            DL = 180 - FL -C
+            BL = 90 - DL
+            lWebcamXValue = (BL + 45)*webcamColumns/cameraMaxHorzAngle
+
+            if (lWebcamXValue < 640) and (lWebcamXValue>=0):
+                leftMap[r,c,0] = lWebcamYValue
+                leftMap[r,c,1] = lWebcamXValue
+                compArray[0,r,c] = int(left[lWebcamYValue,lWebcamXValue])
+                leftInverseMap[lWebcamYValue,lWebcamXValue] = x
+#                
+#                    midCompArray[0,r-hArray/4,c-wArray/4] = left[lWebcamYValue,lWebcamXValue]
                     
-#                    print lWebcamXValue + "," + lWebcamYValue
-#                    print rWebcamXValue + "," + rWebcamYValue
-               
-                	#this is wrong, but close, need to throw in a factor for max vertical angle
-            #    	lWebcamYValue = (yvalue/depthRows)*webcamRows
+            E = 180 - C
+            wR = math.sqrt(math.pow(x,2)+math.pow(separation,2) - 2*x*separation*math.cos(math.radians(E)))
+            FR = math.degrees(math.asin(separation*math.sin(math.radians(E))/wR))
+            DR = 180 - FL - E
+            BR = DR - 90
+            rWebcamXValue = (BR + 45)*webcamColumns/cameraMaxHorzAngle
+            if rWebcamXValue < 640 and rWebcamXValue>=0:
+                rightMap[r,c,0] = rWebcamYValue
+                rightMap[r,c,1] = rWebcamXValue
+                compArray[1,r,c] = int(right[rWebcamYValue,rWebcamXValue])
+                rightInverseMap[rWebcamYValue,rWebcamXValue] = x
+            if abs(compArray[0,r,c]-compArray[1,r,c]<70):
+                success+=1
+                if (r>hArray/4)and(r<hArray*3/4)and(c>wArray/4)and(c<wArray*3/4):
+                    middle50Success+=1
+                if (r>3*hArray/8)and(r<hArray*5/8)and(c>wArray*3/8)and(c<wArray*5/8):
+                    middle25Success+=1
+            else:
+                errors[r,c,0]=255;
+                    
+    print loops
+    successRate = float(success)/(hArray*wArray)
+    print successRate
+    mid50SuccessRate = float(middle50Success)/((hArray*wArray)/4)
+    print mid50SuccessRate
+    mid25SuccessRate = float(middle25Success)/((hArray*wArray)/16)
+    print mid25SuccessRate
+    successHistory = float(successRate+(successHistory*(loops-1)))/loops
+    print successHistory
+    mid50SuccessHistory = float(mid50SuccessRate+(mid50SuccessHistory*(loops-1)))/loops
+    print mid50SuccessHistory
+    mid25SuccessHistory = float(mid25SuccessRate+(mid25SuccessHistory*(loops-1)))/loops
+    print mid25SuccessHistory
+    
+    dualMap = ((compArray[0,:,:]+compArray[1,:,:])/2)
+    dualMap = cv2.cvtColor(dualMap,cv2.COLOR_GRAY2RGB)
+    
+    dualMap[errors.nonzero(),:]=[255,0,0];    
+    
+    cv2.imshow(dualMap)    
     
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
+#        depthimagefile+=1;
+#        scaler = 255
+#        depthArray = depthArray*scaler
+#        filename = 'C:/ADEV/WinPython-64bit-2.7.10.3/python-2.7.10.amd64/Scripts/ADEV/depthImages/' + str(depthimagefile) + '.tiff'
+#        cv2.imwrite(filename, depthArray)
 #  
 #    	
 #    		
 #    	
 #  
-print float(success)/(hArray*(wArray-50))
+
 cv2.destroyWindow("Left")
 cv2.destroyWindow("Right")
 cv2.destroyWindow('DepthImage')
